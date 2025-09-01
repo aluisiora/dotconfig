080a2906ebff6d1b64080952c3d5c24a373ffd34
Aluisio Amaral <aluisioramtl@gmail.com>
2025-08-31 23:03:00 -0300

    chore(sddm): updated scaling config on apple studio display

--- nixos/apple-studio-display.nix
+++ nixos/apple-studio-display.nix
@@ -15,7 +15,7 @@
   services.displayManager.sddm.enableHidpi = true;
   services.displayManager.sddm.settings = {
     General = {
-      GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2";
+      GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2,XCURSOR_SIZE=48";
     };
   };
 
