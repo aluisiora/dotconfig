d662a7d4a68abafa2490442ce94b85549830daff
Aluisio Amaral <aluisioramtl@gmail.com>
2025-09-02 10:36:35 -0300

    chore(nixos): added flatpak and flathub

--- nixos/desktop/desktop.nix
+++ nixos/desktop/desktop.nix
@@ -77,6 +77,15 @@
   services.dbus.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   services.xserver = {
     xkb = {
       layout = "us";
