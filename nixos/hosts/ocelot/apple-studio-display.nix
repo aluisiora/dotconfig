10eb0de3bb3c02af462a30927797d34b76707080
Aluisio Amaral <aluisioramtl@gmail.com>
2025-09-23 16:10:14 -0300

    fix(nixos): removed scaling envs

--- nixos/hosts/ocelot/apple-studio-display.nix
+++ nixos/hosts/ocelot/apple-studio-display.nix
@@ -4,14 +4,6 @@
   ...
 }:
 {
-  environment.variables = {
-    GDK_SCALE = 1;
-    GDK_DPI_SCALE = 2;
-    QT_SCALE_FACTOR = 1;
-    QT_SCREEN_SCALE_FACTORS=1;
-    XCURSOR_SIZE = 24;
-  };
-
   services.xserver.dpi = 218;
   services.displayManager.sddm.enableHidpi = true;
 
